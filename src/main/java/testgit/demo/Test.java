c338373a5702747ff008d20942cd64cf49a0e751
hushuangshuang <249402190@qq.com>
2021-08-10 11:13:26 +0800

    Initial commit

---
+++ src/main/java/testgit/demo/Test.java
@@ -0,0 +1,14 @@
+package testgit.demo;
+
+/**
+ * @author shuang
+ * @version 1.0
+ * @date 2021/8/10/0010
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello gitHub");
+        System.out.println("hello gitHub111");
+    }
+}
