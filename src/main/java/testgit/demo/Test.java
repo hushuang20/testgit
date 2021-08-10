e29bafd1a9b34a0cc49989d53fc5944b8365313d
hushuang20 <68887539+hushuang20@users.noreply.github.com>
2021-08-10 11:26:23 +0800

    Update Test.java

--- src/main/java/testgit/demo/Test.java
+++ src/main/java/testgit/demo/Test.java
@@ -10,5 +10,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello gitHub");
         System.out.println("hello gitHub111");
+        System.out.println("hello gitHub111);
     }
 }
